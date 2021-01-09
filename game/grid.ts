193baa3c696c5515fd034fb5ae94d3ac77b91d41
Layne Faler <laynefaler@gmail.com>
2021-01-09 12:27:07 -0800

    Progress: Winning method

--- game/grid.ts
+++ game/grid.ts
@@ -1,7 +1,7 @@
 import { PlayerType } from "./types";
 
 export class Grid {
-    public board: number[][];
+    public board: PlayerType[][];
     
     constructor() {
         this.board = [
@@ -47,14 +47,42 @@ export class Grid {
     }
 
     private checkRows(player: PlayerType): PlayerType {
+        for (let x = 0; x < this.board.length; x++) {
+            if (this.inRow(x, player) !== PlayerType.Empty) return player;
+        }
         return PlayerType.Empty;
     }
 
     private checkColumns(player: PlayerType): PlayerType {
+        for (let y = 0; y < this.board[0].length; y++) {
+            if (this.inColumn(y, player) !== PlayerType.Empty) return player;
+        }
         return PlayerType.Empty;
     }
 
     private checkDiagnials(player: PlayerType): PlayerType {
+        return PlayerType.Empty;
+    }
+
+    private inRow(x: number, player: PlayerType): PlayerType {
+        return this.isIn(this.board[x], player, this.board[0].length - 4);
+    }
+
+    private inColumn(y: number, player: PlayerType): PlayerType {
+        const column: PlayerType[] = [];
+
+        for (let x = 0; x < this.board.length; x++) {
+            column.push(this.get(x, y));
+        }
+
+        return this.isIn(column, player, this.board.length - 4);
+    }
+
+    private isIn(array: PlayerType[], player: PlayerType, offset: number): PlayerType {
+        for (let i = 0; i <= offset; i++) {
+            let temp = array.slice(i, i + 4);
+            if (temp.every(position => position === player)) return player;
+        }
         return PlayerType.Empty;
     }
 }
