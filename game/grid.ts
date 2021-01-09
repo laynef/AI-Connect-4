e1884e1cd46d877279e479ed86a718e0c220ebf9
Layne Faler <laynefaler@gmail.com>
2021-01-09 12:06:48 -0800

    Progress: winning method

--- game/grid.ts
+++ game/grid.ts
@@ -14,15 +14,15 @@ export class Grid {
         ]
     }
 
-    get(x: number, y: number) {
+    public get(x: number, y: number) {
         return this.board[y][x];
     }
 
-    set(x: number, y: number, player: PlayerType) {
+    public set(x: number, y: number, player: PlayerType) {
         this.board[x][y] = player;
     }
 
-    move(y: number, player: PlayerType) {
+    public move(y: number, player: PlayerType) {
         if (this.get(0, y) !== PlayerType.Empty) throw Error("No available spaces in this column");
 
         for (let x = this.board.length; x >= 0; x--) {
@@ -31,5 +31,30 @@ export class Grid {
                 break;
             }
         }
+    }
+
+    public isWinner(player: PlayerType): PlayerType {
+        let won: PlayerType = this.checkRows(player);
+        if (won !== PlayerType.Empty) return won;
+
+        won = this.checkColumns(player);
+        if (won !== PlayerType.Empty) return won;
+
+        won = this.checkDiagnials(player);
+        if (won !== PlayerType.Empty) return won;
+
+        return won;
+    }
+
+    private checkRows(player: PlayerType): PlayerType {
+        return PlayerType.Empty;
+    }
+
+    private checkColumns(player: PlayerType): PlayerType {
+        return PlayerType.Empty;
+    }
+
+    private checkDiagnials(player: PlayerType): PlayerType {
+        return PlayerType.Empty;
     }
 }
